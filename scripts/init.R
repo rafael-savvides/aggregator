459a4ffdadfb0b692f590d0ac6bc229cdbdc6ae1
Rafael Savvides <savvidesrafael@gmail.com>
2021-04-17 18:24:04 +0300

    Delete read_raw_data.R load_data.R utils.R init.R

--- scripts/init.R
+++
@@ -1,9 +0,0 @@
-suppressPackageStartupMessages(source("../R/load_data.R"))
-suppressPackageStartupMessages(source("../R/read_raw_data.R"))
-suppressPackageStartupMessages(source("../R/utils.R"))
-data_dict = load_data_dictionary("../data/paths_to_raw_data/", 
-                                 readLines("../data/path_to_clean_dir.txt")) 
-data_dict 
-
-message("* `data_dict` lists available data in a data frame . \n", 
-        "* `load_(name)` loads a dataset. ")
