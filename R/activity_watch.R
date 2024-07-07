eb4e2f16f1e168e2de9bef9941dd31e3d281b700
Rafael Savvides <savvidesrafael@gmail.com>
2024-07-07 12:37:22 +0300

    Reorganize functions in files
    After the previous refactoring, each .R file does not need to correspond to a dataset.

---
+++ R/activity_watch.R
@@ -0,0 +1,98 @@
+library(jsonlite)
+library(dplyr)
+library(tidyr)
+library(stringr)
+
+#' Read ActivityWatch data
+#' 
+#' Exported data from ActivityWatch are in json format.
+#'
+#' @param path_to_json 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+read_activity_watch_window <- function(path_to_json) {
+    buckets = jsonlite::read_json(path_to_json)[[1]] |> 
+    tibble(watchers = .) |> 
+    unnest_wider(watchers)
+    window = buckets |> 
+      filter(str_detect(id, "window")) |> 
+      select(events) |> 
+      unnest_longer(events) |> 
+      unnest_wider(events) |> 
+      unnest_wider(data)
+    window
+}
+
+#' Read ActivityWatch data
+#' 
+#' Exported data from ActivityWatch are in json format.
+#'
+#' @param path_to_json 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+read_activity_watch_web <- function(path_to_json) {
+    buckets = jsonlite::read_json(path_to_json)[[1]] |> 
+    tibble(watchers = .) |> 
+    unnest_wider(watchers)
+    web = buckets |> 
+      filter(str_detect(id, "web")) |> 
+      select(events) |> 
+      unnest_longer(events) |> 
+      unnest_wider(events) |> 
+      unnest_wider(data)
+    web
+}
+
+
+
+#' Read ActivityWatch data
+#' 
+#' Exported data from ActivityWatch are in json format.
+#'
+#' @param path_to_json 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+read_activity_watch_afk <- function(path_to_json) {
+    buckets = jsonlite::read_json(path_to_json)[[1]] |> 
+    tibble(watchers = .) |> 
+    unnest_wider(watchers)
+    afk = buckets |> 
+      filter(str_detect(id, "afk")) |> 
+      select(events) |> 
+      unnest_longer(events) |> 
+      unnest_wider(events) |> 
+      unnest_wider(data)
+    afk
+}
+
+#' Read ActivityWatch data
+#' 
+#' Exported data from ActivityWatch are in json format.
+#'
+#' @param path_to_json 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+read_activity_watch_vscode <- function(path_to_json) {
+    buckets = jsonlite::read_json(path_to_json)[[1]] |> 
+    tibble(watchers = .) |> 
+    unnest_wider(watchers)
+    vscode = buckets |> 
+      filter(str_detect(id, "vscode")) |> 
+      select(events) |> 
+      unnest_longer(events) |> 
+      unnest_wider(events) |> 
+      unnest_wider(data)
+    vscode
+}
